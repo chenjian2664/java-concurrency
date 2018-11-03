f9581998eb7ae54cf3e5666c358ad474cb050898
chenjian2664 <15068762664@163.com>
2018-11-03 18:58:58 +0800

    add annoations

---
+++ concurrency/src/main/java/com/learn/concurrency/annoations/ThreadSafe.java
@@ -0,0 +1,17 @@
+package com.learn.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by chenjian
+ * 2018/11/3 18:53
+ * 课程里用来标记【线程安全的类】
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+    String value() default "";
+}
